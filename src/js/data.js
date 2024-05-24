32a9b45fb76e93973f497d3c82b31bbac5e407db
Denis-K0 <160408077+Denis-K0@users.noreply.github.com>
2024-05-24 13:51:19 +0200

    Umstrukturierung der "isKingInCheck" Funktion

--- src/js/data.js
+++ src/js/data.js
@@ -9,12 +9,12 @@ export const coreData = {
     player01: 'Player01',
     player02: 'AI',
     board: [],
-    getPlayerName: function() {
+    getPlayerName() {
         coreData.player01 = document.getElementById('player01').value || 'Player01';
         coreData.player02 = document.getElementById('player02').value || 'AI';
     },
     // Update the Board Array depending on the board in the DOM
-    updateBoard: function() {
+    updateBoard() {
         const htmlBoard = document.querySelectorAll('.fieldCluster');
         coreData.board = [[], [], [], [], [], [], [], []];
         let i = 0;
@@ -33,41 +33,71 @@ export const coreData = {
 
         console.log(coreData);
 
-        // checkGameStatus():
+        if(gameStatus.checkGameStatus()) return;
         // checkAiTurn(); if no addEventsToBoard
         selectors.addEventsToBoard();
     },
-    countRounds: function() {
+    countRounds() {
         coreData.round += 1;
     },
-    // Parameter 'king' & 'board' are needed if called by filterInvalidMoves()
-    isKingInCheck(king = `king01${selectingData.enemyColor}`, board = coreData.board) {
+
+};
+
+export const gameStatus = {
+    checkGameStatus() {
+        const playerKing = (coreData.round % 2 === 0) ? 'king01Black' : 'king01White'
+        const player = (coreData.round % 2 === 0) ? coreData.player02 : coreData.player01;
+        if(gameStatus.isKingInCheck(playerKing, coreData.board)) {
+            // if(checkMate()) return true;
+            alert(player + " is in Check!");
+            return false;
+        };
+    },
+    // Arguments 'king' & 'board' are always necessary
+    isKingInCheck(king, board) {
         const dangerColor = king.includes('Black') ? 'White' : 'Black';   // Enemy Color
         const pieceColor = (dangerColor === 'Black') ? 'White' : 'Black'; // Own Color
         
-        // Detect all Enemies in the Board.
+        // If one Move of the Enemy get the own King in danger
+        if(gameStatus.detectAllEnemies(king, board, dangerColor, pieceColor)) return true;
+
+        return false;
+    },
+    // Detect all Enemies in the Board
+    detectAllEnemies(king, board, dangerColor, pieceColor) {
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
                 if (board[i][j].includes(dangerColor)) {
-                    // Collect all possible Moves of the Enemy
-                    let dangerId = board[i][j];
-                    let dangerName =  selectingData.getPieceName(dangerId);
-                    let dangerPosition = [i, j];
-                    let kingPosition = selectingData.getPiecePosition(king, board);
-
-                    const { possibleMoves, rochade } = showPieceMovements[dangerName](pieceColor, dangerPosition, 
-                        board, dangerColor, dangerId, coreData.check);
-
-                    // Control if one Move match to the Position of the King
-                    for (const move of possibleMoves) {
-                        if (move[0] === kingPosition[0] && move[1] === kingPosition[1]) {
-                            console.log("Zug entfernt");
-                            return true;
-                        };
-                    };
+                    if(gameStatus.collectAllMoves(king, board, dangerColor, pieceColor, i, j)) return true;
                 };
             };
         };
         return false;
+    },
+    // Collect all possible Moves from the Enemy
+    collectAllMoves(king, board, dangerColor, pieceColor, i, j) {
+        let dangerId = board[i][j];
+        let dangerName =  selectingData.getPieceName(dangerId);
+        let dangerPosition = [i, j];
+        let kingPosition = selectingData.getPiecePosition(king, board);
+
+        const { possibleMoves, rochade } = showPieceMovements[dangerName]
+        (pieceColor, dangerPosition, board, dangerColor, dangerId, coreData.check);
+        
+        if(gameStatus.controlMoveMatch(possibleMoves, kingPosition)) return true;
+        return false;
+    },
+    // Control if one Move match to the Position of the King
+    controlMoveMatch(possibleMoves, kingPosition) {
+        for (const move of possibleMoves) {
+            if (move[0] === kingPosition[0] && move[1] === kingPosition[1]) {
+                console.log("Zug entfernt");
+                return true;
+            };
+        };
+        return false;
+    },
+    checkMate() {
+
     },
 };
--- src/js/gameLogic/pieceMove.js
+++ src/js/gameLogic/pieceMove.js
@@ -1,4 +1,4 @@
-import { coreData } from "../data";
+import { coreData, gameStatus } from "../data";
 import { showPieceMovements } from "./pieces";
 "use strict";
 
@@ -52,6 +52,130 @@ export const selectors = {
     },
 };
 
+export const selectingData = {
+    pieceId: '',
+    availableMoves: [],
+    rochade: false,
+    get pieceName() {
+        return this.getPieceName(this.pieceId);
+    },
+    get piecePosition() {
+        return this.getPiecePosition(this.pieceId, coreData.board);
+    },
+    get pieceColor() {
+        return this.pieceId.includes('White') ? 'White' : 'Black';
+    },
+    get enemyColor() {
+        return this.pieceColor === 'White' ? 'Black' : 'White';
+    },
+    getPieceName(pieceId) {
+        const zeroIndex = pieceId.indexOf('0');
+        return pieceId.slice(0, zeroIndex);
+    },
+    getPiecePosition(pieceId, board) {
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (board[i][j] === pieceId) {
+                    return [i, j];
+                }
+            }
+        };
+        throw new Error("Piece not found on the board! - F:getPiecePosition");
+    },
+};
+
+const selectPieceFunctions = {
+    selectPiece(event) {
+        const cluster = event.currentTarget;
+        selectingData.pieceId = cluster.children[0]?.id ?? 'No Piece in Cluster';
+        console.log(selectingData.pieceId);
+        
+        // First: Check if the Field is valid to be selected from the Player
+        if(!selectPieceFunctions.checkValidTurn(selectingData.pieceId)) return console.log('Valid Turn Success'); 
+        console.log('Valid Turn Success');
+        // Second: Calculate the possible moves & store them
+        selectPieceFunctions.calculateValidMoves(selectingData.pieceName);
+    },
+    checkValidTurn(pieceId) {
+        if(coreData.round % 2 !== 0) {
+            if(pieceId.includes('White')) return true;
+        };
+    
+        if(coreData.round % 2 === 0) {
+            if(pieceId.includes('Black')) return true;
+        };
+    
+        return false;
+    },
+    calculateValidMoves(pieceName) {
+        const { possibleMoves, rochade } = showPieceMovements[pieceName](selectingData.enemyColor, selectingData.piecePosition, 
+            coreData.board, selectingData.pieceColor, selectingData.pieceId, coreData.check);
+            
+        console.log('Rochade => ' + rochade);
+        
+        //Store the rochade value, for the validation & placement
+        selectingData.rochade = rochade;
+        
+        // Filter the possibleMoves, if they get in conflict with a danger for the own king
+        selectingData.availableMoves = selectPieceFunctions.filterInvalidMoves(possibleMoves);
+        
+        if(selectingData.availableMoves.toString() === '') return console.log('No Available Turns');
+        console.log('Avaiable Moves', ...selectingData.availableMoves);
+        
+        selectors.displayValidMoves();
+    },
+    filterInvalidMoves(possibleMoves) {
+        const filteredMoves = [];
+        // Control by each possible move, if a Check is given 
+        for(const move of possibleMoves) {
+            let newBoard = selectPieceFunctions.simulateMove(move, selectingData.pieceId, selectingData.piecePosition);
+            if(!gameStatus.isKingInCheck(`king01${selectingData.pieceColor}`, newBoard)) {
+                filteredMoves.push(move);
+            } else console.log('Zug entfernt! ->' + move);
+        };
+        return filteredMoves;
+    },
+    // Simulate a board with the to executing Move
+    simulateMove(move, pieceId, piecePosition) {
+        const [targedRow, targedCol] = move;
+        const [currentRow, currentCol] = piecePosition;
+        let createdBoard = JSON.parse(JSON.stringify(coreData.board));
+        createdBoard[targedRow][targedCol] = pieceId;
+        createdBoard[currentRow][currentCol] = '';
+        
+        // If Rochade ist true, then apply it correctly
+        if(selectingData.rochade && (targedCol === 3 || targedCol === 5)) {
+            createdBoard = selectPieceFunctions.correctBoardForRochade(createdBoard, selectingData.rochade);
+        };
+
+        console.log("SimulatedBoard->", createdBoard)
+        return createdBoard;
+    },
+    // Only necessary for the Board-Simulation
+    correctBoardForRochade(createdBoard, towerId) {
+        switch (towerId) {
+            case "tower01Black":
+                createdBoard[0][2] = 'king01Black';
+                createdBoard[0][4] = '';
+                return createdBoard;
+            case "tower02Black":
+                createdBoard[0][6] = 'king01Black';
+                createdBoard[0][4] = '';
+                return createdBoard;
+                case "tower01White":
+                    createdBoard[7][2] = 'king01White';
+                createdBoard[7][4] = '';
+                return createdBoard;
+            case "tower02White":
+                createdBoard[7][6] = 'king01White';
+                createdBoard[7][4] = '';
+                return createdBoard;
+                default:
+                    return console.log("Invalid towerId");
+        };
+    },
+};
+                    
 const turnExecution = {
     queenBlack: document.getElementById('queen01Black').cloneNode(true),
     queenWhite: document.getElementById('queen01White').cloneNode(true),
@@ -126,129 +250,6 @@ const turnExecution = {
             pieceElement.remove();
             turnExecution.queenBlack.id = 'queen02Black';
             targetCluster.appendChild(turnExecution.queenBlack);
-        };
-    },
-};
-
-export const selectingData = {
-    pieceId: '',
-    availableMoves: [],
-    rochade: false,
-    get pieceName() {
-        return this.getPieceName(this.pieceId);
-    },
-    get piecePosition() {
-        return this.getPiecePosition(this.pieceId, coreData.board);
-    },
-    get pieceColor() {
-        return this.pieceId.includes('White') ? 'White' : 'Black';
-    },
-    get enemyColor() {
-        return this.pieceColor === 'White' ? 'Black' : 'White';
-    },
-    getPieceName(pieceId) {
-        const zeroIndex = pieceId.indexOf('0');
-        return pieceId.slice(0, zeroIndex);
-    },
-    getPiecePosition(pieceId, board) {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                if (board[i][j] === pieceId) {
-                    return [i, j];
-                }
-            }
-        };
-        throw new Error("Piece not found on the board! - F:getPiecePosition");
-    },
-};
-
-const selectPieceFunctions = {
-    selectPiece(event) {
-        const cluster = event.currentTarget;
-        selectingData.pieceId = cluster.children[0]?.id ?? 'No Piece in Cluster';
-        console.log(selectingData.pieceId);
-    
-        // First: Check if the Field is valid to be selected from the Player
-        if(!selectPieceFunctions.checkValidTurn(selectingData.pieceId)) return console.log('Valid Turn Success'); 
-        console.log('Valid Turn Success');
-        // Second: Calculate the possible moves & store them
-        selectPieceFunctions.calculateValidMoves(selectingData.pieceName);
-    },
-    checkValidTurn(pieceId) {
-        if(coreData.round % 2 !== 0) {
-            if(pieceId.includes('White')) return true;
-        };
-    
-        if(coreData.round % 2 === 0) {
-            if(pieceId.includes('Black')) return true;
-        };
-    
-        return false;
-    },
-    calculateValidMoves(pieceName) {
-        const { possibleMoves, rochade } = showPieceMovements[pieceName](selectingData.enemyColor, selectingData.piecePosition, 
-            coreData.board, selectingData.pieceColor, selectingData.pieceId, coreData.check);
-
-        console.log('Rochade => ' + rochade);
-
-        //Store the rochade value, for the validation & placement
-        selectingData.rochade = rochade;
-        
-        // Filter the possibleMoves, if they get in conflict with a danger for the own king
-        selectingData.availableMoves = selectPieceFunctions.filterInvalidMoves(possibleMoves);
-
-        if(selectingData.availableMoves.toString() === '') return console.log('No Available Turns');
-        console.log('Avaiable Moves', ...selectingData.availableMoves);
-
-        selectors.displayValidMoves();
-    },
-    filterInvalidMoves(possibleMoves) {
-        const filteredMoves = [];
-        // Control by each possible move, if a Check is given 
-        for(const move of possibleMoves) {
-            let newBoard = selectPieceFunctions.simulateMove(move, selectingData.pieceId, selectingData.piecePosition);
-            if(!coreData.isKingInCheck(`king01${selectingData.pieceColor}`, newBoard)) {
-                filteredMoves.push(move);
-            } else console.log('Zug entfernt! ->' + move);
-        };
-        return filteredMoves;
-    },
-    // Simulate a board with the to executing Move
-    simulateMove(move, pieceId, piecePosition) {
-        const [targedRow, targedCol] = move;
-        const [currentRow, currentCol] = piecePosition;
-        let createdBoard = JSON.parse(JSON.stringify(coreData.board));
-        createdBoard[targedRow][targedCol] = pieceId;
-        createdBoard[currentRow][currentCol] = '';
-
-        // If Rochade ist true, then apply it correctly
-        if(selectingData.rochade && (targedCol === 3 || targedCol === 5)) {
-            createdBoard = selectPieceFunctions.correctBoardForRochade(createdBoard, selectingData.rochade);
-        };
-
-        console.log("SimulatedBoard->", createdBoard)
-        return createdBoard;
-    },
-    correctBoardForRochade(createdBoard, towerId) {
-        switch (towerId) {
-            case "tower01Black":
-                createdBoard[0][2] = 'king01Black';
-                createdBoard[0][4] = '';
-                return createdBoard;
-            case "tower02Black":
-                createdBoard[0][6] = 'king01Black';
-                createdBoard[0][4] = '';
-                return createdBoard;
-            case "tower01White":
-                createdBoard[7][2] = 'king01White';
-                createdBoard[7][4] = '';
-                return createdBoard;
-            case "tower02White":
-                createdBoard[7][6] = 'king01White';
-                createdBoard[7][4] = '';
-                return createdBoard;
-            default:
-                return console.log("Invalid towerId");
         };
     },
 };
